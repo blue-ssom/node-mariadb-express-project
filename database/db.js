dbc4edf69be36d41aacbf41ff1e4a158de5b8b38
blue-ssom <somi5404@naver.com>
2024-03-01 16:45:57 +0000

    change database selection for the project

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const connection = maria.createConnection({
     host : 'localhost', // mariaDB가 존재하는 서버의 주소
     user : 'stageus',   // mariaDB의 계정
     password: '1234',   // mariaDB 계정의 비밀번호
-    database : 'scheduler'  // 접속 후 사용할 DB
+    database : 'web'  // 접속 후 사용할 DB
 })
 
 module.exports = connection
