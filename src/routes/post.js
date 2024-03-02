58afaea45262ffb5cefd680f11c81586d7d26a89
blue-ssom <somi5404@naver.com>
2024-03-02 01:49:04 +0000

    implement API for adding posts

--- src/routes/post.js
+++ src/routes/post.js
@@ -45,7 +45,54 @@ router.get('/all', async(req, res) => {
 
 // 게시글 추가하기
 router.post('/', async(req, res) => {
+    const sessionUserIdx = req.session.userIdx; // 세션에 저장된 사용자 idx
+    console.log("게시글 추가하기 세션: ", sessionUserIdx)
+    
+    const { title, content } = req.body
+    const result = {
+        "success" : false,
+        "message" : "",
+        "data" : null
+    }
 
+    try {
+
+        // 예외처리
+        // if (!sessionUserIdx) {
+        //   throw new Error("잘못된 접근입니다.")   // 세션이 없는 경우
+        // } 
+
+        if(title === null || title === undefined || title === ""){
+            throw new Error("제목을 입력해주세요.")
+        }else if(content === null || content === undefined || content === ""){
+            throw new Error("내용을 입력해주세요.")
+        }
+
+        // DB통신
+        const createPostResult = await new Promise((resolve, reject) => {
+            maria.query('INSERT INTO posts (user_idx, title, content, creationDate, updationDate) VALUES (?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)', [sessionUserIdx, title, content], (err, results) => {
+                if (err) {
+                    reject(err); // 오류 발생 시 reject 호출
+                } else {
+                    resolve(results); // 결과 반환 시 resolve 호출
+                }
+            });
+        });
+
+        // DB 통신 결과 처리
+        // affectedRows는 삽입된 행의 수
+        if (createPostResult.affectedRows > 0) {
+            result.success = true;
+            result.message = "게시글 추가하기";
+        } else {
+            result.message = "게시글 작성 실패";
+        }
+        
+    } catch(e) {
+        result.message = e.message;
+    } finally {
+        res.send(result);
+    }
 });
 
 // 게시글 수정하기
